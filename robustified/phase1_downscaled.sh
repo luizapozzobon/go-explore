076c14b29396e60b6f5dd4f4d991b5a9020e20d7
Luiza Pozzobon <luiza.pozzobon@gmail.com>
2022-05-12 16:53:28 +0000

    fix: minor fixes to make phase1_downscale run

--- robustified/phase1_downscaled.sh
+++ robustified/phase1_downscaled.sh
@@ -15,4 +15,4 @@ game=${1:-MontezumaRevenge}
 results=${2:-results}
 frames=${3:-500000000}
 
-python goexplore_py/main.py --seen_weight=1.0 --reset_cell_on_update --base_path=$results --game=generic_$game --cell_split_factor=0.125 --first_compute_archive_size=1 --first_compute_dynamic_state=10000 --max_archive_size=50000 --max_recent_frames=10000 --recent_frame_add_prob=0.01 --recompute_dynamic_state_every=10000000 --split_iterations=3000 --state_is_pixels --high_score_weight=0.0 --max_hours=256 --max_compute_steps=$frames --n_cpus=88 --batch_size=100 --dynamic_state
+python main.py --seen_weight=1.0 --reset_cell_on_update --base_path=$results --game=generic_$game --cell_split_factor=0.125 --first_compute_archive_size=1 --first_compute_dynamic_state=10000 --max_archive_size=50000 --max_recent_frames=10000 --recent_frame_add_prob=0.01 --recompute_dynamic_state_every=10000000 --split_iterations=3000 --state_is_pixels --high_score_weight=0.0 --max_hours=256 --max_compute_steps=$frames --n_cpus=88 --batch_size=100 --dynamic_state
