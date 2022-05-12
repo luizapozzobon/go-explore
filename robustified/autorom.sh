d5138d8bbf33c75ec0b423acd94f3cfee0b9553c
Luiza Pozzobon <luiza.pozzobon@gmail.com>
2022-05-12 14:10:28 +0000

    add environment installer helpers

---
+++ robustified/autorom.sh
@@ -0,0 +1,2 @@
+AutoROM --install-dir /home/luiza.pozzobon/miniconda3/envs/go-explore-seed/lib/python3.6/site-packages/atari_py/atari_roms
+
---
+++ robustified/horovod.sh
@@ -0,0 +1,2 @@
+pip install tensorflow==1.15.2
+HOROVOD_CUDA_HOME=/usr/local/cuda/bin/cuda pip install --no-cache-dir horovod==0.19.1
