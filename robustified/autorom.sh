cdabfce1f1d3b4d884ec8c37572109e0540e7f4d
Luiza Pozzobon <luiza.pozzobon@gmail.com>
2022-05-19 15:44:28 -0300

    fix: modify environment and installation scripts to work with docker environment

--- robustified/autorom.sh
+++ robustified/autorom.sh
@@ -1,2 +1,2 @@
-AutoROM --install-dir /home/luiza.pozzobon/miniconda3/envs/go-explore-seed/lib/python3.6/site-packages/atari_py/atari_roms
+AutoROM --install-dir /root/miniconda3/envs/go-explore-seed/lib/python3.6/site-packages/atari_py/atari_roms
 
--- robustified/horovod.sh
+++ robustified/horovod.sh
@@ -1,2 +1,4 @@
 pip install tensorflow==1.15.2
-HOROVOD_CUDA_HOME=/usr/local/cuda/bin/cuda pip install --no-cache-dir horovod==0.19.1
+pip install tensorflow-gpu==1.15.2
+# HOROVOD_CUDA_HOME=/usr/local/cuda/bin/cuda pip install --no-cache-dir horovod==0.19.1
+HOROVOD_GPU_ALLREDUCE=NCCL pip install --no-cache-dir horovod==0.19.1
