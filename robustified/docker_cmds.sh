c7678aab5c06c5d3a36bfa611f17f98d1e143c31
Luiza Pozzobon <luiza.pozzobon@gmail.com>
2022-05-19 15:57:08 -0300

    fix: remove docker_cmds.sh

--- robustified/docker_cmds.sh
+++
@@ -1,1 +0,0 @@
-nvidia-docker run -ti --userns=host --shm-size 64G -v /home/luiza.pozzobon/repos/submodule/atari:/atari/ -p 9042:9042 --name luizapozzobon_atari-env luiza.pozzobon/atari-env:latest /bin/bash
